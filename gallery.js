559d1c582cbb36278c75a558c1d677c157cc35cf
Jason Zheng <jazheng@davidson.edu>
2020-02-20 10:10:36 -0800

    v2 with js

--- gallery.js
+++ gallery.js
@@ -2,12 +2,37 @@ let fs = require('fs');
 
 let imageFileNames = fs.readdirSync('site/images');
 
-// This code is just here to demonstrate how fs.readdirSync works
+let head = [
+  '<!DOCTYPE html>',
+  '<html>',
+  '<head>',
+  '<title>Photo Gallery</title>',
+  '<link rel = "stylesheet" href = "css/main.css">',
+  '</head>',
+  '<body>',
+  '<h1>The Photo Gallery</h1>',
+];
 
-console.log('sites/images contains the following files:')
-
-for (let i = 0; i < imageFileNames.length; i++) {
-  let fileName = imageFileNames[i];
-
-  console.log(`${i + 1}. ${fileName}`);
+let imageArray = [];
+for (let image of imageFileNames) {
+  let imageSyntax = '<img src = "../site/images/' + image + '" alt = "">';
+  imageArray.push(imageSyntax);
 }
+
+let photo = [
+  '<section class = "image">',
+];
+
+for (let image of imageArray) {
+  photo.push(image);
+}
+photo.push('</section>');
+
+let foot = [
+  '</body>',
+  '</html>',
+];
+
+let htmlDocument = head.join('\n') + '\n' + photo.join('\n') + '\n' + foot.join('\n');
+
+fs.writeFileSync('./site/index.html', htmlDocument);
